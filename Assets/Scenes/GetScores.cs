466c36d999c7ca60f5b35bd2904e3a60e77c9344
Markus <Markuspiotrowski@live.com>
2021-09-27 10:44:21 +0200

    SavePre

---
+++ Assets/Scenes/GetScores.cs
@@ -0,0 +1,18 @@
+using System.Security.AccessControl;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GetScores
+{
+    public bool first;
+    public float[] joystickScores;
+    public float[] buttonScores;
+    public float[] gyroScores;
+
+    public GetScores()
+    {
+
+    }
+}
---
+++ Assets/Scenes/SaveScores.cs
@@ -0,0 +1,43 @@
+using System.Diagnostics;
+using System;
+using System.Security.AccessControl;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+public static class SaveScores
+{
+    public static void SaveData()
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/player.fbx";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        GetScores scores = new GetScores();
+
+        formatter.Serialize(stream, scores);
+        stream.Close();
+
+    }
+    public static GetScores LoadScores()
+    {
+        string path = Application.persistentDataPath + "/player.fbx";
+        if (File.Exists(path))
+        {
+            FileStream stream = new FileStream(path, FileMode.Open);
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            GetScores scores = formatter.Deserialize(stream) as GetScores;
+            stream.Close();
+            return scores;
+
+        }
+        else
+        {
+            UnityEngine.Debug.LogError("Save file not found in " + path);
+            return null;
+        }
+    }
+
+
+}
